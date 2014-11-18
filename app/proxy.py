1a850b69ec6d0978434d7504af5c3e6ac7c5d15a
Zachary Yaro <zmyaro@gmail.com>
2014-11-18 02:09:17 -0500

    Add proxy for iTunes and Spotify search APIs

---
+++ app/proxy.py
@@ -0,0 +1,33 @@
+import cgi
+import urllib
+import webapp2
+
+ITUNES_SEARCH_URL = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term='
+SPOTIFY_SEARCH_URL = 'https://api.spotify.com/v1/search?type=track&q='
+
+class ITunesSearch(webapp2.RequestHandler):
+	def get(self):
+		# Do not allow usage by arbitrary external pages.
+		if 'Referer' not in self.request.headers or self.request.headers['Referer'].find(self.request.host_url) == -1:
+			self.error(403)
+			return
+		# Fetch and return the JSON from iTunes.
+		self.response.headers['Content-Type'] = 'application/json'
+		resp = urllib.urlopen(ITUNES_SEARCH_URL + self.request.get('q'))
+		self.response.write(resp.read())
+
+class SpotifySearch(webapp2.RequestHandler):
+	def get(self):
+		# Do not allow usage by arbitrary external pages.
+		if 'Referer' not in self.request.headers or self.request.headers['Referer'].find(self.request.host_url) == -1:
+			self.error(403)
+			return
+		# Fetch and return the JSON from Spotify.
+		self.response.headers['Content-Type'] = 'application/json'
+		resp = urllib.urlopen(SPOTIFY_SEARCH_URL + self.request.get('q'))
+		self.response.write(resp.read())
+
+app = webapp2.WSGIApplication([
+	('/proxy/itunes', ITunesSearch),
+	('/proxy/spotify', SpotifySearch)
+], debug=True)
